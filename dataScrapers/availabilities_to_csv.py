09e32b7b713171e91fd0d7c111ded2d26f5f9d17
nikolaspistiolas <npistiolas4@gmail.com>
2022-05-10 15:02:36 +0300

    balancing requirements

---
+++ dataScrapers/availabilities_to_csv.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import os
+import datetime
+import pytz
+
+
+def get_dts(start_date, end_date, delta):
+    print(start_date.month)
+    if is_dst_change(start_date.replace(tzinfo=None), 'Europe/Athens'):
+        if start_date.month == 3:
+            dts = [dt for dt in datetime_range(start_date, end_date, delta, skip=True)]
+
+        elif start_date.month == 10:
+            print('HERE')
+            dts = [dt for dt in datetime_range(start_date, end_date, delta, add_twice=True)]
+    else:
+        dts = [dt for dt in datetime_range(start_date, end_date, delta)]
+    return dts
+
+
+def datetime_range(start, end, delta, hour=2, skip=False, add_twice=False):
+    current = start
+
+    while current < end:
+        if skip:
+            if current.hour != hour:
+                yield current.replace(tzinfo=None)
+        elif add_twice:
+            if current.hour == hour:
+                yield current.replace(tzinfo=None)
+                yield current.replace(tzinfo=None)
+            else:
+                yield current.replace(tzinfo=None)
+        else:
+            yield current.replace(tzinfo=None)
+        current += delta
+
+
+def is_dst_change(day: datetime.datetime, timezone):
+    # Override time to midnight
+    day = day.replace(hour=0, minute=0, second=0, microsecond=0)
+    tz = pytz.timezone(timezone)
+    return tz.utcoffset(day + datetime.timedelta(days=1)) != tz.utcoffset(day)
+
+
+files = os.listdir('./unit_availabilities')
+
+files = sorted(files)[1:]
+
+gen_df = pd.DataFrame()
+data = []
+
+for f in files:
+    try:
+        df = pd.read_excel(f'./unit_availabilities/{f}', skiprows=3, usecols=[2, 3])
+        date = f.split('_')[0]
+        year = int(date[:4])
+        month = int(date[4:6])
+        day = int(date[6:])
+        date = datetime.datetime(year,month,day)
+        diff = df[df.columns.values[0]].sum() - df[df.columns.values[1]].sum()
+
+        data.append([date,diff])
+    except:
+        break
+
+
+gen_df = pd.DataFrame.from_records(data)
+gen_df.columns = ['Date', 'Values']
+gen_df = gen_df.set_index('Date')
+gen_df.to_csv('unit_availabilities.csv')
+
+
+
